be90ebb4be70c40c8f01f945cd7151112c840fd7
Katya <fox_cat@mail.ru>
2025-05-20 09:53:16 +0300

    фикс имя пользователя

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -10,7 +10,7 @@ def test_post_v1_account():
     account_api = AccountApi(host = 'http://5.63.153.31:5051')
     login_api= LoginApi(host='http://5.63.153.31:5051')
     mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
-    login = 'katya_1_2365_12'
+    login = 'katya_1_2365_13'
     password = '123456789'
     email = f'{login}@mail.ru'
     json_data = {
