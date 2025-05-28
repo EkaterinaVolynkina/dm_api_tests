c3f69ceff93a616c327822bd9b076e8e72c6d629
Katya <fox_cat@mail.ru>
2025-05-28 12:36:52 +0300

    + auth_client

--- helpers/account_helper.py
+++ helpers/account_helper.py
@@ -33,6 +33,23 @@ class AccountHelper:
         assert response.status_code == 200, 'Активация пользователя не удалась'
         return response
 
+    def auth_client(
+            self,
+            login: str,
+            password: str
+    ):
+        response = self.dm_account_api.login_api.post_v1_account_login(
+            json_data={
+                'login': login,
+                'password': password
+            }
+        )
+        token = {
+            'x-dm-auth-token': response.headers['x-dm-auth-token']
+        }
+        self.dm_account_api.account_api.set_headers(token)
+        self.dm_account_api.login_api.set_headers(token)
+
     def user_login(
             self,
             login: str,
