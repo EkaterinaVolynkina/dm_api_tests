9ed907bcf20e3c84bda891e2aa349a93b8ed45be
Katya <fox_cat@mail.ru>
2025-06-06 12:42:45 +0300

    Написала негативные тесты для метода post_v1_account

--- checkers/http_checkers.py
+++ checkers/http_checkers.py
@@ -1,17 +1,44 @@
 from contextlib import contextmanager
-
 import requests
-from requests import HTTPError
-
+from requests.exceptions import HTTPError
 
 @contextmanager
-def check_status_code_http(expected_status_code: requests.codes = requests.codes.OK, expected_message: str = ''):
+def check_status_code_http(
+    expected_status_code: int = requests.codes.ok,
+    expected_message: str = ''
+):
     try:
         yield
-        if expected_status_code != requests.codes.OK:
-            raise AssertionError(f'Ожидаемый статус код должен быть равен {expected_status_code}')
-        if expected_status_code != requests.codes.OK:
-            raise AssertionError(f'Должно быть получено сообщение "{expected_message}", но запрос прошел успешно')
+        # Если ожидаем ошибку, но она не произошла
+        if expected_status_code != requests.codes.ok:
+            raise AssertionError(
+                f'Ожидалась ошибка с кодом {expected_status_code}, но запрос завершился успешно'
+            )
     except HTTPError as e:
-        assert e.response.status_code == expected_status_code
-        assert e.response.json()['title'] == expected_message
+        # Если произошла ошибка, но мы ожидали успех
+        if expected_status_code == requests.codes.ok:
+            raise AssertionError(
+                f'Ожидался успешный ответ, но получена ошибка: {e.response.status_code} - {e.response.text}'
+            )
+        # Проверка кода ошибки
+        assert e.response.status_code == expected_status_code, \
+            f'Ожидался статус {expected_status_code}, получен {e.response.status_code}'
+        # Проверка сообщения
+        if expected_message:
+            actual_message = e.response.json().get('title', '')
+            assert actual_message == expected_message, \
+                f'Ожидалось сообщение "{expected_message}", получено "{actual_message}"'
+# @contextmanager
+# def check_status_code_http(
+#         expected_status_code: requests.codes = requests.codes.OK,
+#         expected_message: str = ''
+#         ):
+#     try:
+#         yield
+#         if expected_status_code != requests.codes.OK:
+#             raise AssertionError(f'Ожидаемый статус код должен быть равен {expected_status_code}')
+#         if expected_status_code != requests.codes.OK:
+#             raise AssertionError(f'Должно быть получено сообщение "{expected_message}", но запрос прошел успешно')
+#     except HTTPError as e:
+#         assert e.response.status_code == expected_status_code
+#         assert e.response.json()['title'] == expected_message
--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,3 +1,8 @@
+import pytest
+
+from checkers.http_checkers import check_status_code_http
+from dm_api_account.models.registration import Registration
+
 
 def test_post_v1_account(
         account_helper,
@@ -8,3 +13,19 @@ def test_post_v1_account(
     password = prepare_user.password
     email = prepare_user.email
     account_helper.register_new_user(login=login, password=password, email=email)
+
+
+@pytest.mark.parametrize('login, email, password, expected_status_code', [
+    ('user1', 'email@mail.ru', '123', 400),
+    ('user2', 'emailmail.ru', 'Qwerty123!', 400),
+    ('k', 'email@mail.ru', 'Qwerty123!', 400)
+])
+def test_post_v1_account_negative(account_helper, login, email, password, expected_status_code):
+    registration = Registration(
+        login=login,
+        email=email,
+        password=password
+    )
+
+    with check_status_code_http(expected_status_code):
+        account_helper.dm_account_api.account_api.post_v1_account(registration=registration)
