4103b913c7eee921b29d62738b7fa8a152e26301
Ekaterina <fox_cat@mail.ru>
2025-06-13 11:21:00 +0300

    Update test_delete_v1_account_login_all.py

--- tests/functional/delete_v1_account_login_all/test_delete_v1_account_login_all.py
+++ tests/functional/delete_v1_account_login_all/test_delete_v1_account_login_all.py
@@ -5,10 +5,6 @@ def test_delete_v1_account_login_all(auth_account_helper):
     with check_status_code_http():
         auth_account_helper.delete_login_all()
 
-<<<<<<< pydantic_1306
 def test_delete_v1_account_login_negative_all(account_helper):
-=======
-def test_delete_v1_account_login_negative(account_helper):
->>>>>>> main
     with check_status_code_http(401, 'User must be authenticated'):
         account_helper.delete_login_all()
