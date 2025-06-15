f17295f947a8bc6fd3772b1694a1ced58d8af30d
Katya <fox_cat@mail.ru>
2025-06-15 14:40:09 +0300

    Telegram_bot_html

--- packages/notifier/bot.py
+++ packages/notifier/bot.py
@@ -4,15 +4,12 @@ from telebot import TeleBot
 from vyper import v
 
 
-config_path = Path(__file__).resolve().parent.parent.parent / 'config'
-v.set_config_name('prod')  # имя файла без расширения
-v.add_config_path(str(config_path))
+config = Path(__file__).resolve().parent.parent.parent / 'config'
+v.set_config_name('prod')
+print(config)
+print(config.is_dir())
+v.add_config_path(config)
 v.read_in_config()
-
-# config = Path(__file__).joinpath('../../').joinpath('config')
-# v.set_config_name('prod')
-# v.add_config_path(config)
-# v.read_in_config()
 
 os.environ['TELEGRAM_BOT_CHAT_ID'] = v.get('telegram.chat_id')
 os.environ['TELEGRAM_BOT_ACCESS_TOKEN'] = v.get('telegram.token')
@@ -20,7 +17,7 @@ os.environ['TELEGRAM_BOT_ACCESS_TOKEN'] = v.get('telegram.token')
 
 def send_file() -> None:
     telegram_bot = TeleBot(v.get('telegram.token'))
-    file_path = Path(__file__).parent.joinpath('../../').joinpath('swagger-coverage-config-dm-api-account.json')
+    file_path = Path(__file__).parent.joinpath('../../').joinpath('swagger-coverage-dm-api-account.html')
     with open(file_path, 'rb') as document:
         telegram_bot.send_document(
             v.get('telegram.chat_id'),
